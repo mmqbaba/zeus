4c9573f2b0c1630cbdb6c141135faa4f10196939
markmai <markmai@digitalgd.com.cn>
2019-11-27 15:48:16 +0800

    Add # 添加redis lock

---
+++ lock/redis/lock_test.go
@@ -0,0 +1,49 @@
+package lock
+
+import (
+	"context"
+	"testing"
+
+	"gitlab.dg.com/BackEnd/jichuchanpin/tif/zeus/config"
+	redisclient "gitlab.dg.com/BackEnd/jichuchanpin/tif/zeus/redis"
+)
+
+func TestObtain(t *testing.T) {
+	rdc := redisclient.InitClient(&config.Redis{
+		Host: "127.0.0.1:6379",
+	})
+	type args struct {
+		ctx    context.Context
+		client RedisClient
+		key    string
+		opts   *Options
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Locker
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			"obtain_lock",
+			args{
+				context.Background(),
+				rdc.GetCli(),
+				"unittest:lock:",
+				nil,
+			},
+			&Locker{},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := Obtain(tt.args.ctx, tt.args.client, tt.args.key, tt.args.opts)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Obtain() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
