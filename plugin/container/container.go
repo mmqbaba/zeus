d098859eccff06135c0cdeb05a7d2301bfaef273
mark <markmai@digitalgd.com.cn>
2020-04-02 16:37:46 +0800

    Add # 增加container providor自定义扩展

--- plugin/container/container.go
+++ plugin/container/container.go
@@ -1,11 +1,58 @@
 package container
 
 import (
+	"context"
+	"fmt"
+	"strings"
+
 	"gitlab.dg.com/BackEnd/jichuchanpin/tif/zeus/plugin"
 	"gitlab.dg.com/BackEnd/jichuchanpin/tif/zeus/plugin/zcontainer"
 )
 
+func init() {
+	containerProvidors = map[string]NewContainerFn{
+		CATEGORY_ZEUS: NewZeusContainer,
+	}
+}
+
+// CATEGORY_ZEUS 框架默认提供的containerprovidor名称
+const CATEGORY_ZEUS = "ZEUS"
+
+var containerProvidors map[string]NewContainerFn
+
+type NewContainerFn func(ctx context.Context) zcontainer.Container
+
+// GetContainer 返回框架默认提供的container
 func GetContainer() zcontainer.Container {
+	return NewContainer(context.Background(), CATEGORY_ZEUS)
+}
+
+// NewZeusContainer plugin.NewContainer框架默认提供的containerprovidor
+func NewZeusContainer(ctx context.Context) zcontainer.Container {
 	cnt := plugin.NewContainer()
 	return cnt
+}
+
+// NewContainer 创建指定的container
+func NewContainer(ctx context.Context, categoryName string) zcontainer.Container {
+	name := categoryName
+	if len(strings.TrimSpace(categoryName)) == 0 {
+		name = CATEGORY_ZEUS
+	}
+	p, ok := containerProvidors[name]
+	if !ok || p == nil {
+		panic(fmt.Errorf("unsupport container category: %s", name))
+	}
+	return p(ctx)
+}
+
+// RegistryProvidor 注册容器提供者，用于扩展containerprovidor
+func RegistryProvidor(name string, providor NewContainerFn) {
+	if len(strings.TrimSpace(name)) == 0 {
+		panic(fmt.Errorf("name can not be empty"))
+	}
+	_, ok := containerProvidors[name]
+	if ok {
+		panic(fmt.Errorf("the container providor:%s was already existed", name))
+	}
 }
