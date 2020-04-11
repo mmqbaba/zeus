aa992510512069f1064235e0321da2b310090186
mark <markmai@digitalgd.com.cn>
2020-04-11 16:49:49 +0800

    Change # updater gin v1.5.0

--- tools/gen-zeus/generator/generator_go_mod.go
+++ tools/gen-zeus/generator/generator_go_mod.go
@@ -20,7 +20,7 @@ replace (
 
 require (
 	github.com/coreos/etcd v3.3.17+incompatible
-	github.com/gin-gonic/gin v1.3.0
+	github.com/gin-gonic/gin v1.5.0
 	github.com/golang/protobuf v1.3.2
 	github.com/grpc-ecosystem/grpc-gateway v1.12.0
 	github.com/micro/go-micro v1.18.0
